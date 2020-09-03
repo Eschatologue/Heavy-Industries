4f7400631b7012fd8dcb17a7af360462f3d77000
ItzAlen <alen2001valentino@gmail.com>
2020-09-03 09:50:47 +0700

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,9 +1,10 @@
 // Rotary Compressor
 const rotaryCompressor = extendContent(GenericCrafter, "rotary-compressor", {
   load(){
-    this.bottomRegion = Core.atlas.find(this.name + "-bottom");
-    this.rotatorRegion = Core.atlas.find(this.name + "-rotator");
-    this.topRegion = Core.atlas.find(this.name + "-top");
+	this.bottomRegion = Core.atlas.find(this.name + "-bottom");
+	this.rotatorRegion = Core.atlas.find(this.name + "-rotator");
+	this.topRegion = Core.atlas.find(this.name + "-top");
+
   },
   generateIcons: function(){
     return [
@@ -12,10 +13,10 @@ const rotaryCompressor = extendContent(GenericCrafter, "rotary-compressor", {
   },
   draw(tile){
     const entity = tile.ent();
-    const f = Vars.tilesize;
-    Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
-    Draw.rect(this.rotatorRegion, tile.drawx() + 0 / f, tile.drawy() - 0 / f, entity.totalProgress * 2);
-    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+	const f = Vars.tilesize;
+	Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
+	Draw.rect(this.rotatorRegion, tile.drawx() + 0 / f, tile.drawy() - 0 / f, entity.totalProgress * -2);
+	Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
   },
 });
  //Combustion Turbine Generator
